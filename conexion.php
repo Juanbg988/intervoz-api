43896b066878f4400fc40753a575831b4e794b73
Juan de Jesús Bautista García <L23350591@tuxtepec.tecnm.mx>
2026-05-24 16:56:28 -0600

    conexion con variables rail

--- conexion.php
+++ conexion.php
@@ -4,12 +4,14 @@ $host = getenv("MYSQL_HOST");
 $user = getenv("MYSQL_USER");
 $pass = getenv("MYSQL_PASSWORD");
 $db   = getenv("MYSQL_DATABASE");
+$port = getenv("MYSQL_PORT") ?: 3306;
 
 $conn = mysqli_connect(
     $host,
     $user,
     $pass,
-    $db
+    $db,
+    $port
 );
 
 if (!$conn) {
